3dde6a9f1dbfc37c276b7e86594c4fcf9ad840a1
dmarcios <denilsonmarcio@hotmail.com>
2023-11-03 18:28:44 -0300

    Add Dockerfile

--- LocadoraVHS/src/main/java/br/univesp/projeto/locadora/LocadoraApplication.java
+++ LocadoraVHS/src/main/java/br/univesp/projeto/locadora/LocadoraApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LocadoraVhsApplication {
+public class LocadoraApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LocadoraVhsApplication.class, args);
+		SpringApplication.run(LocadoraApplication.class, args);
 	}
 
 }
